92f0305657ea5c9e9d79ee317790421fa75822d3
Charlie Ward <59178341+Charlie-Ward@users.noreply.github.com>
2023-10-06 12:32:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,53 @@
 # Importing random
 import random
+from array import *
 
 # Adding base numbers
 numbers = [1,2,3,4]
 
 # Setting up the base board
-board = []
+board = [[0],[0],[0],[0]]
 
+# Shuffling numbers for the first row
 numbers_shuffled = random.sample(numbers, len(numbers))
-board.append(numbers_shuffled)
-print(board)
+for i in range(len(numbers_shuffled)):
+    board[0].append(numbers_shuffled[i])
+board[0].remove(0)
 row_1 = board[0]
-print(row_1)
+
+# First square
+unavailable_numbers = [board[0][0],board[0][1]]
+numbers_temp = [1,2,3,4]
+for i in range(len(unavailable_numbers)):
+    pop = unavailable_numbers[i]
+    numbers_temp.remove(pop)
+numbers_temp = random.sample(numbers_temp,len(numbers_temp))
+for i in range(len(numbers_temp)):
+    board[1].append(numbers_temp[i])
+board[1].remove(0)
+
+# Second square
+unavailable_numbers = random.sample(unavailable_numbers, len(unavailable_numbers))
+for i in range(len(unavailable_numbers)):
+    board[1].append(unavailable_numbers[i])
+
+# First column
+unavailable_numbers = [board[0][0],board[1][0]]
+numbers_temp = [1,2,3,4]
+for i in range(len(unavailable_numbers)):
+    pop = unavailable_numbers[i]
+    numbers_temp.remove(pop)
+numbers_temp = random.sample(numbers_temp, len(numbers_temp))
+for i in range(len(numbers_temp)):
+    board[i+2].append(numbers_temp[i])
+    board[i+2].remove(0)
+
+# Second Column
+unavailable_numbers = random.sample(unavailable_numbers, len(unavailable_numbers))
+for i in range(len(unavailable_numbers)):
+    board[i+2].append(unavailable_numbers[i])
+
+print(board[0])
+print(board[1])
+print(board[2])
+print(board[3])
